015de8232da773823baa441c13b43cc3ca4640b7
Jade Lovelace <lix@jade.fyi>
2024-03-22 20:09:22 -0700

    java somewhat harder

--- flake.nix
+++ flake.nix
@@ -26,6 +26,24 @@
             ]);
             runScript = "zsh";
           }).env;
+
+          packages.default = pkgs.maven.buildMavenPackage {
+            pname = "keycloak-lists-plugin";
+            version = "1.0";
+
+            src = ./plugin;
+
+            mvnHash = "sha256-UaVCt6KIjR8i3vHVp5YWqu8zzM7mftXyrv5J2jxtw6Q=";
+
+            buildPhase = ''
+              mvn --offline package;
+            '';
+
+            installPhase = ''
+              mkdir -p $out/share/java
+              install -Dm644 target/*.jar $out/share/java
+            '';
+          };
         };
     in
     flake-utils.lib.eachDefaultSystem out // {
--- plugin/src/main/java/lix/systems/keycloak/FileAllowBansDB.java
+++ plugin/src/main/java/lix/systems/keycloak/FileAllowBansDB.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
